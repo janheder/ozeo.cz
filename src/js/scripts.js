858d956ab65dd361c260ece754917cc45e15c5ab
Jan Heder <info@janheder.cz>
2021-06-13 19:04:36 +0200

    day 4

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -48,3 +48,118 @@ document.getElementById('darkBackdrop').onclick = function() {
 }
 
 
+var inc = document.getElementsByClassName("stepper");
+
+if (inc.length > 0){
+    
+    for (abc = 0; abc < inc.length; abc++) {
+    var incI = inc[abc].querySelector("input"),
+        id = incI.getAttribute("id"),
+        min = incI.getAttribute("min"),
+        max = incI.getAttribute("max"),
+        step = incI.getAttribute("step");
+    document
+        .getElementById(id)
+        .previousElementSibling.setAttribute(
+        "onclick",
+        "stepperInput('" + id + "', -" + step + ", " + min + ")"
+        ); 
+    document
+        .getElementById(id)
+        .nextElementSibling.setAttribute(
+        "onclick",
+        "stepperInput('" + id + "', " + step + ", " + max + ")"
+        ); 
+    }
+
+    function stepperInput(id, s, m) {
+        var el = document.getElementById(id);
+        if (s > 0) {
+            if (parseInt(el.value) < m) {
+            el.value = parseInt(el.value) + s;
+            }
+        } else {
+            if (parseInt(el.value) > m) {
+            el.value = parseInt(el.value) + s;
+            }
+        }
+    }
+
+}
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+
+    var carousel = document.getElementsByClassName('product-detail__carousel');
+
+    if (carousel.length > 0){
+
+
+        const $imagesContainer = document.getElementById('product-carousel');
+        const $lightbox = document.getElementById('lightbox');
+     
+        $imagesContainer.addEventListener('mousedown', (event) => {
+            clickTime = new Date();
+        });
+        $imagesContainer.addEventListener('mouseup', e => {
+            if (new Date() - clickTime < 150) {
+                const imageWrapper = e.target.closest('.product-detail__carouselLink');
+                if (imageWrapper) {
+                    const image = imageWrapper.querySelector('img');
+                    if (image) {
+                        $lightbox.innerHTML = '<div class="close-lightbox"></div>' + image.outerHTML;
+                        $lightbox.classList.add('show');
+                    }
+                }
+              } else {
+                // pause
+              }
+    
+        });
+    
+        $lightbox.addEventListener('click', (e) => {
+            if (!e.target.hasAttribute('src')) {
+                $lightbox.classList.remove('show');
+            }
+        });
+
+
+      
+        const thumbCarousel = new Siema({
+            selector: '.product-detail__carousel',
+            duration: 200,
+            easing: 'ease-out',
+            perPage: 1,
+            startIndex: 0,
+            draggable: true,
+            multipleDrag: true,
+            threshold: 20,
+            loop: true,
+            rtl: false,
+            onInit: () => {},
+            onChange: () => {},
+        });
+    
+        // Add a function that generates pagination to prototype
+        Siema.prototype.addPagination = function() {
+            for (let i = 0; i < this.innerElements.length; i++) {
+            const thumb = document.getElementById("thumb" + i);
+            thumb.addEventListener('click', () => this.goTo(i));
+            }
+        }
+        
+        
+        // Trigger pagination creator
+        thumbCarousel.addPagination();
+
+    }
+});
+
+
+
+
+
